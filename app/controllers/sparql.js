05cea43c492eedcde76b312becc4f0bd050a6dbd
Aad Versteden <madnificent@gmail.com>
2025-07-04 23:35:44 +0200

    Remove fastboot
    It's not needed in this app and we shouldn't bother with the maintenance
    involved.

--- app/controllers/sparql.js
+++ app/controllers/sparql.js
@@ -1,6 +1,4 @@
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default class SparqlController extends Controller {
-  @service fastboot;
 }
